8361cbcc756758a2ad4dcb06e4c800efac457c25
ryuclover <gabrielzlrx@gmail.com>
2026-09-10 18:08:42 -0300

    Trace Playwright browsers metadata in API

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,12 @@ const nextConfig = {
   },
   serverExternalPackages: ["puppeteer", "playwright-core", "@sparticuz/chromium"],
   outputFileTracingIncludes: {
+    "/api/buscar": [
+      "./node_modules/@sparticuz/chromium/**/*",
+      "./node_modules/playwright-core/browsers.json",
+      "./node_modules/playwright-core/package.json",
+      "./node_modules/playwright-core/lib/**/*",
+    ],
     "/api/**/*": [
       "./node_modules/@sparticuz/chromium/**/*",
       "./node_modules/playwright-core/**/*",
